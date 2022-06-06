4df315b9edae387ddc187b6bd323af44674b612d
emrahsusam <emrahsusam01@gmail.com>
2022-06-06 19:00:50 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -1,26 +1,30 @@
-let adSoyad = prompt("Lütfen Adınızı Girin");
 
-
+//Prompt Question- What's your name?
+let sayName = prompt("Lütfen Adınızı Girin");
+//Your Answer - Name
 let info = document.getElementById("myName");
-info.innerHTML = `${info.innerHTML}${adSoyad}`
+info.innerHTML = `${info.innerHTML}${sayName}`
 
+   //Weekdays 
+   let weekdays = new Array(7);
+   weekdays[0] = "Pazar";
+   weekdays[1] = "Pazartesi";
+   weekdays[2] = "Salı";
+   weekdays[3] = "Çarşamba";
+   weekdays[4] = "Perşembe";
+   weekdays[5] = "Cuma";
+   weekdays[6] = "Cumartesi";
+
+//Digital Clock Function
 function showTime() {
     let date = new Date();
     let h = date.getHours(); // 0 - 23
     let m = date.getMinutes(); // 0 - 59
     let s = date.getSeconds(); // 0 - 59
-    
+    let day = weekdays[date.getDay()]; // Weekdays in Turkish
 
-    //Weekdays 
-    let weekdays = new Array(7);
-        weekdays[0] = "Pazar";
-        weekdays[1] = "Pazartesi";
-        weekdays[2] = "Salı";
-        weekdays[3] = "Çarşamba";
-        weekdays[4] = "Perşembe";
-        weekdays[5] = "Cuma";
-        weekdays[6] = "Cumartesi";
-    let day = weekdays[date.getDay()];
+ 
+    
     
 
 //Double writing not => 1-2-3.. like => 01-02-03..    
@@ -28,10 +32,11 @@ function showTime() {
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
 
+    // Show clock function 
     let time = h + ":" + m + ":" + s + " " + day;
     document.getElementById("myClock").innerHTML = time;
   
-  let t = setTimeout(function(){ showTime() }, 1000);
+  setTimeout(function(){ showTime() }, 1000);
 }
 
 showTime();
