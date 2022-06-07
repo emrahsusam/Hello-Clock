253af1734b466aee707c4ad3a10b00a9ab6fb0e2
emrahsusam <emrahsusam01@gmail.com>
2022-06-07 06:42:00 +0300

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -21,13 +21,10 @@ function showTime() {
     let h = date.getHours(); // 0 - 23
     let m = date.getMinutes(); // 0 - 59
     let s = date.getSeconds(); // 0 - 59
-    let day = weekdays[date.getDay()]; // Weekdays in Turkish
-
- 
-    
+    let day = weekdays[date.getDay()]; // Weekdays in Turkish    
     
 
-//Double writing not => 1-2-3.. like => 01-02-03..    
+//Ternary writing not => 1-2-3.. like => 01-02-03..    
     h = (h < 10) ? "0" + h : h;
     m = (m < 10) ? "0" + m : m;
     s = (s < 10) ? "0" + s : s;
